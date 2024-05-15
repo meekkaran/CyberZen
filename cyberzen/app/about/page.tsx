fe9c62b54a87a4ec9cfec6535f86794bfcde6660
meekkaran <meekkaran@gmail.com>
2024-05-15 22:19:10 +0300

    main body

---
+++ cyberzen/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import * as React from "react";
+
+const About: React.FC = () => {
+  return (
+    <Box>
+        
+    </Box>
+  );
+};
+
+export default About;
--- cyberzen/app/page.tsx
+++ cyberzen/app/page.tsx
@@ -1,15 +1,23 @@
-
-
+import Image from 'next/image';
 import Footer from "@/components/Footer";
 import Header from "@/components/Header"
+import { Grid, Typography } from "@mui/material";
 
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <h1>I am so adorable</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography variant="h2"> Empowering businesses through insightful consultancy and innovative software solutions.</Typography>
+          <Typography variant="h3">"We specialize in consultancy and software solutions.</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Image src="/images/backgroundimg.jpg" alt="Your Image" width={500} height={300} />
+        </Grid>
+      </Grid>
       <Footer />
-    </div>
+    </>
   );
 }
