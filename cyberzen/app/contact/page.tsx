4d8489890d928ade2ed61a8f1e509edb7bf3d1d9
meekkaran <meekkaran@gmail.com>
2024-05-27 13:34:43 +0300

    working on contact us page?

--- cyberzen/app/contact/page.tsx
+++ cyberzen/app/contact/page.tsx
@@ -1,11 +1,23 @@
-
 import React from 'react';
+import {Typography, Grid, Box } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+
 
 const ContactPage: React.FC = () => {
   return (
     <div>
-      <h1>Contact Us</h1>
-      <p>This is the contact page content.</p>
+        <Grid container spacing={2} p={8} alignItems="center" justifyContent="center" direction="column">
+          <Typography variant="h1" sx={{ color: '#0d0c22', textAlign: 'center', fontSize: '4rem', fontWeight: 'bold' }}>Got A Question?</Typography>
+          <Typography variant="h6" sx={{ textAlign: 'center' }}>Leave us a message below and we'll get in touch with you as soon as we can.</Typography>
+      </Grid>
+
+      <Grid container spacing={2}>
+      <Grid item xs={6} md={8}>
+        
+      </Grid>
+    </Grid>
+      
+
     </div>
   );
 };
